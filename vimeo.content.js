a119744caec64df48f3a761a7dda6fc8f1651d82
spacerest <sadiemparker@gmail.com>
2020-08-18 22:50:12 +0300

    hotkeys for increase/decrease speed and scrub forward/backward by 1 second

--- vimeo.content.js
+++ vimeo.content.js
@@ -1,9 +1,5 @@
-
-// Screenshot from	https://vimeo.com/35132562
-// Or				http://www.dailymotion.com/video/x2dntv_back-to-life-back-to-reality_fun
-// Or				https://www.youtube.com/watch?v=_Xmu-DOTChE
-
 function loadConfig() {
+	//console.debug('[vimeo hotkeys] Load config');
 	return new Promise(function(resolve) {
 		if (chrome.storage) {
 			chrome.storage.local.get(['config'], function(items) {
@@ -23,112 +19,26 @@ function onTimeUpdate(e) {
 	}
 }
 
-function createXButton($box, onClick) {
-	var $button = $box.querySelector('button');
-	$button.onclick = onClick;
+function createChangePosition(video) {
 
-	$button.onmouseover = function() {
-		with ( this.previousElementSibling.classList ) {
-			remove('hidden');
-			remove('invisible');
-			add('visible');
-		}
-	};
-	$button.onmouseout = function() {
-		with ( this.previousElementSibling.classList ) {
-			add('hidden');
-			add('invisible');
-			remove('visible');
-		}
-	};
+	function changePosition(direction) {
+		var position = video.currentTime;
+		var offset = direction * 5;
+		video.currentTime = position - offset + direction;
+		console.debug("Position is " + position + direction);
+	}
 
-	return $button;
+	return [changePosition];
 }
 
-function createRepeatButton(video) {
-	var $box = document.createElement('div');
-	$box.className = 'box';
 
-	var html = '';
-	html += '<style>';
-	html += 'html .player .repeat-button { color: white; font-weight: bold; }\n';
-	html += 'html .player .repeat-button.on { background-color: rgb(0, 173, 239); }\n';
-	html += 'html .player .repeat-button.on:active { background-color: rgb(0, 147, 203); }\n';
-	html += '</style>';
-	html += '<label class="rounded-box repeat-label invisible hidden" role="presentation"><span>Repeat</span></label>';
-	html += '<button title="Click to toggle." tabindex="50" class="repeat-button rounded-box" aria-label="Repeat">REP&#8203;EAT</button>';
-	$box.innerHTML = html;
-
-	var $button = createXButton($box, function(e) {
-		var repeating = this.classList.toggle('on');
-		if ( repeating ) {
-			video.addEventListener('timeupdate', onTimeUpdate);
-		}
-		else {
-			video.removeEventListener('timeupdate', onTimeUpdate);
-		}
-	});
-
-	return [$box];
-}
-
-function createSpeedButton(video) {
-	var $box = document.createElement('div');
-	$box.className = 'box';
-
-	var html = '';
-	html += '<style>';
-	html += 'html .player .speed-button { color: white; font-weight: bold; } \n';
-	html += 'html .player .speed-button.on { background-color: rgb(0, 173, 239); } \n';
-	html += 'html .player .speed-button.on:active { background-color: rgb(0, 147, 203); } \n';
-	html += '</style>';
-	html += '<label class="rounded-box speed-label invisible hidden" role="presentation"><span>Playback rate</span></label>';
-	html += '<button title="Mouse scroll to change speed. Click to enter custom speed." tabindex="50" class="speed-button rounded-box" aria-label="Speed">' + video.playbackRate + 'x</button>';
-	$box.innerHTML = html;
-
+function createDeltaSpeed(video) {
 	const speeds = [0.2, 0.3333, 0.5, 0.6666, 1, 1.25, 1.5, 2, 2.5, 3, 4];
 
-	function setLabel() {
-		$button.textContent = (Math.round(video.playbackRate * 100) / 100) + 'x';
-	}
-
 	function setSpeed(speed) {
+		console.debug("setting speed to " + speed);
 		video.playbackRate = speed;
-
-		if ( video.playbackRate == 1 ) {
-			$button.classList.remove('on');
-		}
-		else {
-			$button.classList.add('on');
-		}
-
-		// Fake trigger "events-moused-over"
-		var $dock = document.querySelector('.vp-sidedock');
-		$dock.classList.remove('hidden');
-		$dock.classList.remove('invisible');
-		$dock.hidden = false;
 	}
-
-	var $button = createXButton($box, function(e) {
-		var curSpeed = video.playbackRate;
-		var newSpeed = prompt('New playback rate:', curSpeed);
-		if ( newSpeed === null ) return;
-
-		newSpeed = parseFloat(newSpeed.replace(/,/g, '.'));
-		if ( !isNaN(newSpeed) && newSpeed != curSpeed ) {
-			setSpeed(newSpeed);
-		}
-	});
-	$button.onwheel = function(e) {
-		e.preventDefault();
-
-		var direction = e.deltaY < 0 ? 1 : -1;
-		deltaSpeed(direction);
-	};
-
-	video.addEventListener('ratechange', function(e) {
-		setLabel();
-	});
 
 	function deltaSpeed(direction) {
 		var curSpeed = video.playbackRate;
@@ -151,48 +61,66 @@ function createSpeedButton(video) {
 		}
 	}
 
-	return [$box, setSpeed, deltaSpeed];
+	return [deltaSpeed];
 }
 
-
-
 function tryToInitPlayer(attemptsLeft, $player) {
-	var $buttons = $player.querySelector('.vp-controls-wrapper .vp-sidedock, .controls-wrapper .sidedock');
+	//var $buttons = $player.querySelector('.vp-controls-wrapper .vp-sidedock, .controls-wrapper .sidedock');
 	var $video = $player.querySelector('video');
+        //console.debug('video is ' + $video);
+        //console.debug('player is ' + $player);
 
-	if ( $player && $buttons && $video ) {
-		// Only do once!
-		if ( !$buttons.querySelector('.repeat-button') ) {
-			console.debug('[Vimeo Repeat] Adding buttons');
+	if ( $player && $video ) {
+	// Only do once!
+		//console.debug("there's a player and a video");
 
-			// Add REPEAT button
-			const [$box1] = createRepeatButton($video);
-			$buttons.appendChild($box1);
+		const [deltaSpeed] = createDeltaSpeed($video);
+		const [changePosition] = createChangePosition($video);
+		// $buttons.appendChild($box2);
 
-			// Add SPEED button
-			const [$box2, setSpeed, deltaSpeed] = createSpeedButton($video);
-			$buttons.appendChild($box2);
-
-			document.addEventListener('keydown', function(e) {
-				if ( !e.altKey && !e.ctrlKey ) {
-					if ( e.code === 'Minus' ) {
-						deltaSpeed(-1);
-					}
-					else if ( e.code === 'Equal' ) {
-						deltaSpeed(+1);
-					}
+		// clue on how to keep keydown from looping:
+		// https://stackoverflow.com/a/17514833/5650506
+		var $down = false;
+		document.addEventListener('keydown', function(e) {
+			console.debug("key down");
+			if ( !($speedChanged) && !($down) && !e.altKey && !e.ctrlKey ) {
+				if ( !($speedChanged) && e.code === 'Minus' ) {
+					$speedChanged = true;
+					deltaSpeed(-1);
 				}
-			});
-
-			loadConfig().then(config => {
-				if ( config.defaultSpeed ) {
-					setSpeed(config.defaultSpeed);
+				else if ( !($speedChanged) && e.code === 'Equal' ) {
+					$speedChanged = true;
+					deltaSpeed(+1);
 				}
-			})
-		}
+				else if ( !($speedChanged) && e.code === 'ArrowRight' ) {
+					$speedChanged = true;
+					changePosition(+1);
+				}
+				else if ( !($speedChanged) && e.code === 'ArrowLeft' ) {
+					$speedChanged = true;
+					changePosition(-1);
+				}
+
+			}
+			$down = true;
+		}, false);
+
+		document.addEventListener('keyup', function() {
+			console.debug("key up");
+			$down = false;
+			$speedChanged = false;
+		}, false);
+
+		loadConfig().then(config => {
+			if ( config.defaultSpeed ) {
+				setSpeed(config.defaultSpeed);
+			}
+		})
 	}
 	else {
-		console.debug(`[Vimeo Repeat] Can't find buttons (${$buttons ? 'Y' : 'N'}), or video (${$video ? 'Y' : 'N'}). Trying ${attemptsLeft} more times.`);
+		//console.debug(`[vimeo hotkeys] $buttons`);
+		//console.debug(`[vimeo hotkeys] $video`);
+		console.debug(`[vimeo hotkeys] Can't find video (${$video ? 'Y' : 'N'}). Trying ${attemptsLeft} more times.`);
 		if ( attemptsLeft > 0 ) {
 			setTimeout(function() {
 				tryToInitPlayer(--attemptsLeft, $player);
@@ -201,13 +129,14 @@ function tryToInitPlayer(attemptsLeft, $player) {
 	}
 }
 
-
-
 var $player = document.querySelector('div.player');
+var $speedChanged = false;
+console.debug("player is " + $player);
 if ( $player ) {
 	var mo = new MutationObserver(function(muts) {
 		// Try to find buttons menu & video element
 		tryToInitPlayer(2, $player);
+		console.debug("There's a player");
 	});
 	mo.observe($player, {"childList": true, "attributes": true});
 }
